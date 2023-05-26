3932d2c8c57c816a6e667802572cb85dc6fa4780
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-05-26 15:21:45 +0200

    Placeholder Navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import StartingPage from "./pages/StartingPage";
 import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [afterAnim, setAfterAnim] = useState(false);
   return (
     <div className="w-screen h-screen">
-      {!afterAnim && <StartingPage setAfterAnim={setAfterAnim} />}
-      {afterAnim && <HomePage/>}
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        {!afterAnim && <StartingPage setAfterAnim={setAfterAnim} />}
+        {afterAnim && <HomePage />}
+      </div>
     </div>
   );
 }
 
 export default App;
-
