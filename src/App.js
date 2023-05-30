45c2c948397b22ff0e17bd4537906008b958051b
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-05-30 19:42:04 +0200

    Layout Changes

--- src/App.js
+++ src/App.js
@@ -9,8 +9,33 @@ function App() {
   return (
     <div className="w-screen h-screen flex flex-col">
       <Navbar />
-      {!afterAnim && <StartingPage setAfterAnim={setAfterAnim} />}
-      {afterAnim && <HomePage />}
+      <div className="flex h-full">
+        <div className="bg-slate-900 basis-1/2 flex flex-col px-8">
+          <div className="text-white text-xl font-fixedsys mt-5 mb-10">
+            Lorem ipsum dolor sit amet.
+          </div>
+          <div className="text-red-300 text-sm font-fixedsys mb-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac
+            venenatis metus, lobortis consequat tortor. Sed quis viverra elit.
+            Quisque sit amet enim sit amet dui ullamcorper pulvinar non vitae
+            turpis. Sed purus dui, dictum ut diam a, fringilla pulvinar libero.
+            Pellentesque feugiat mi lacus, eget vehicula libero facilisis ac.
+            Curabitur pellentesque tempor hendrerit. Pellentesque urna dui,
+            pretium ac lectus sed, maximus aliquet nibh.
+          </div>
+          <div className="flex justify-around text-white">
+            <button>Aaaa</button>
+            <button>Aaaa</button>
+          </div>
+        </div>
+        <div className="basis-1/2 border-2 border-black flex flex-col h-full">
+          <div className="basis-3/4 bg-slate-100">
+            {!afterAnim && <StartingPage setAfterAnim={setAfterAnim} />}
+            {afterAnim && <HomePage />}
+          </div>
+          <div className="grow border-black border-2 bg-black text-white font-fixedsys"> Elo </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='flex w-full justify-evenly items-center border-solid bg-slate-900 text-white font-fixedsys text-2xl '>
+    <div className='flex w-full justify-evenly items-center border-solid bg-slate-900 text-white font-fixedsys text-xl '>
       <div className='flex justify-around'>
         <div className='mx-8 border-white hover:border-b '>Produkty</div>
         <div className='mx-8 border-white hover:border-b'>Galeria</div>
       </div>
       <div>
-        <img className='w-[200px] h-[150px]' src='/logo.png' alt="logo"></img>
+        <img className='w-[150px] h-[112px]' src='/logo.png' alt="logo"></img>
       </div>
       <div className='flex justify-around'>
         <div className='mx-8 border-white hover:border-b'>O nas</div>
